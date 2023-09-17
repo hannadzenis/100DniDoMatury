1c3c5ff19f58fec0ca8b64da7433a361ac061cc3
Miran Shamaev <61889214+CyberMan-00@users.noreply.github.com>
2023-09-18 00:14:45 +0200

    update: promo changed, cta button added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,16 +1,17 @@
+AOS.init();
+
 const swiper = new Swiper(".coursesSwiper", {
     direction: "horizontal",
     slidesPerView: 1,
     spaceBetween: 30,
-    // mousewheel: true,
-
-    // effect: "fade",
     grabcursor: true,
 
-    // effect: "cube",
     pagination: {
         el: ".swiper-pagination",
-        clickable: true
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
     },
 
     // Responsive breakpoints
@@ -41,29 +42,6 @@ const swiper2 = new Swiper(".reviewsSwiper", {
     effect: "cards",
     grabcursor: true,
 
-    // effect: "cube",
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-    },
+    // effect: "flip",
 
-    // // Responsive breakpoints
-    // breakpoints: {
-    //     // when window width is >= 1px
-
-    //     690: {
-    //         slidesPerView: 1,
-    //         spaceBetween: 30
-    //     },
-    //     // when window width is >= 767px
-    //     767: {
-    //         slidesPerView: 2,
-    //         spaceBetween: 30
-    //     },
-    //     // when window width is >= 1140px
-    //     1140: {
-    //         slidesPerView: 3,
-    //         spaceBetween: 10,
-    //     }
-    // }
 });
